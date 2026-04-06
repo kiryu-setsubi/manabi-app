8ec7f89bc4dd3536ebacf1bb7197d52ce63c9a96
kiryu-setsubi <ryuheimiyano@gmail.com>
2026-04-06 20:43:43 +0900

    service-worker.js を更新

--- service-worker.js
+++ service-worker.js
@@ -8,13 +8,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
     )
   );
 });
